1dcbdd4ec58d0cc246c2e0bc746b99ed7c1e7c05
Shreeyav031 <shreeyav031@gmail.com>
2024-09-09 20:42:03 +0530

    HTML DOM

--- javascript.js
+++ javascript.js
@@ -1,17 +1,19 @@
 function validate(e) {
-    e.preventdefault();
+    e.preventDefault();
     const email=document.getElementById
     ("email").value;
     const password=document.getElementById
     ("pass").value;
-var msg=document.getElementById()
+var msg=document.getElementById("message")
 let message='';
-if(email ===''){
+if(email ==='')
+    {
 message = 'please enter an email.';
-}else if (pass ===''){
+}else if (password ===''){
     message='password must have atleast 10 characters.';
 }
 else{
     message='LOGIN SUCCESSFUL';
     }
     msg.innerText=message;
+}
